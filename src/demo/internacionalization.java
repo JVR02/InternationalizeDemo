c3b944e11e1a7ff99cf8320119056cd1105eff07
JVR02 <99564204+JVR02@users.noreply.github.com>
2022-03-28 11:18:31 -0500

    se egrega el proyecto

---
+++ src/demo/internacionalization.java
@@ -0,0 +1,14 @@
+
+package demo;
+import demo.form;
+
+
+public class internacionalization {
+
+
+    public static void main(String[] args) {
+        form f=new form();
+        f.setVisible(true);
+    }
+    
+}
